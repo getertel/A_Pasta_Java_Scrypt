e5e13ef204fd3fc3de4d66791e7d22c78f09b88c
getertel <geter3@hotmail.com>
2024-08-11 01:39:17 -0300

    nomeando tipos e criano arrays e objetos

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+// tipos de variaveis:
+
+let idade = 59       //tipo number
+let nome = "boniec"  //tipo string
+let logado = true    //tipo boolean
+
+// array e objetos
+
+let ingredientes = ["farinha", "agua", "sal", "açucar", "ovos"]
+console.log(ingredientes[1])
+
+let personagem = {
+    nome: "Geter",
+    condicao: "fase dificil",
+    oqueFazer: "Agurdando ajuda de Deus",
+    dores: "Ja imaginando oque há por vir!"  
+}
+
+console.log(personagem.dores, personagem.condicao, ingredientes[2])
