26c4b4bd1557b9009a1831c703c683ffe57bfbae
getertel <geter3@hotmail.com>
2024-08-12 23:06:23 -0300

    Funções, Aerofunction, aplicações

--- assets/script.js
+++ assets/script.js
@@ -6,14 +6,34 @@ let logado = true    //tipo boolean
 
 // array e objetos
 
-let ingredientes = ["farinha", "agua", "sal", "açucar", "ovos"]
+let ingredientes = ["farinha!", "agua", "sal!", "açucar!", "ovos!"]
 console.log(ingredientes[1])
 
 let personagem = {
     nome: "Geter",
-    condicao: "fase dificil",
-    oqueFazer: "Agurdando ajuda de Deus",
-    dores: "Ja imaginando oque há por vir!"  
+    condicao: "fase dificil,",
+    oqueFazer: "Aguardando ajuda de Deus.",
+    dores: "Ja imaginando oque há por vir!",
+    vai: "mãos a obra!"  
 }
 
-console.log(personagem.dores, personagem.condicao, ingredientes[2])
+console.log(personagem.dores, personagem.condicao, ingredientes[2], personagem.vai)
+
+//criando funções
+
+function sum(a, b) {
+    let resultado = a + b;
+    return resultado
+   
+}
+
+//criando uma aero function
+ let subtract = (a, b) => {
+    let resultado = a - b;
+    return resultado
+ }
+let x = sum(25, 60)
+let y = subtract(59, 30)
+
+console.log(x)
+console.log(y)
