9841bb6c93b20da49e22f26ea544a79303c490c0
getertel <geter3@hotmail.com>
2024-08-22 00:47:09 -0300

    Atualizando configurações finais

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,17 @@ let usdInput = document.querySelector("#usd")
 let brlInput = document.querySelector("#brl")
 
 usdInput.addEventListener("keyup", () => {
+   convert("usd-to-brl");
 
-})
+});   
 
 brlInput.addEventListener("keyup", () => {
+  convert("brl-to-usd");
+});
 
-})
+usdInput.value = "1000.00"
+convert("usd-to-brl");
 
-usdInput.value = "1000,00"
-convert("usd-to-brl")
 
 //funções
 function formatCurrency(value) {
@@ -23,33 +25,44 @@ function formatCurrency(value) {
        useGrouping: false,
        minimumFractionDigits: 2
       
-      }
-      let formatter = new Intl.NumberFormat("pt-BR", options)
+      };
+      let formatter = new Intl.NumberFormat("pt-BR", options);
    // retorna o valor formatado
-      return formatter.format(fixedValue)
+      return formatter.format(fixedValue);
 
 }
 
 function fixValue(value) {
-     let fixedValue = value.replace(",", ".")
-     let floatValue = parseFloat(fixedValue)
-     if(floatValue == NaN) {
-      floatValue = 0
+     let fixedValue = value.replace(",", ".");
+     let floatValue = parseFloat(fixedValue);
+     //corrigido para isNaN
+     if(isNaN(floatValue)) {
+      floatValue = 0;
      }
-     return floatValue 
+     return floatValue; 
 }
 
 function convert(type) {
     if(type == "usd-to-brl") {
       // ajustar o valor
+      let fixedValue = fixValue(usdInput.value);
       // converter o valor
+      let result = fixedValue * dolar ;
+      result = result.toFixed(2);
       // mostra no campo de real
+      brlInput.value = formatCurrency(result);
       
     }
     if(type == "brl-to-usd") {
      // ajustar o valor
+     let fixedValue = fixValue(brlInput.value);
       // converter o valor
+     let result = fixedValue / dolar;
+     result = result.toFixed(2);
+
       // mostra no campo de dolar
+      usdInput.value = formatCurrency(result);
+      
 
     }
   }
