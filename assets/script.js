83cff02fc315acfc15b28418b63bfea9d4cdb99b
getertel <geter3@hotmail.com>
2024-08-21 23:53:25 -0300

    funções, Ajustar-o-valor, atualizar-função-de-formatar, retornar-o-valor-formatado

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,55 @@
+let dolar = 5.48
+
 let usdInput = document.querySelector("#usd")
 let brlInput = document.querySelector("#brl")
 
-//teste de campo de uso
 usdInput.addEventListener("keyup", () => {
-   console.log("apertou no campo uso")
+
 })
 
 brlInput.addEventListener("keyup", () => {
-   console.log("apertou no campo BRL")
+
 })
+
+usdInput.value = "1000,00"
+convert("usd-to-brl")
+
+//funções
+function formatCurrency(value) {
+   // ajustar o valor
+   let fixedValue = fixValue(value)
+   //atulizar função de formatar 
+   let options = {
+       useGrouping: false,
+       minimumFractionDigits: 2
+      
+      }
+      let formatter = new Intl.NumberFormat("pt-BR", options)
+   // retorna o valor formatado
+      return formatter.format(fixedValue)
+
+}
+
+function fixValue(value) {
+     let fixedValue = value.replace(",", ".")
+     let floatValue = parseFloat(fixedValue)
+     if(floatValue == NaN) {
+      floatValue = 0
+     }
+     return floatValue 
+}
+
+function convert(type) {
+    if(type == "usd-to-brl") {
+      // ajustar o valor
+      // converter o valor
+      // mostra no campo de real
+      
+    }
+    if(type == "brl-to-usd") {
+     // ajustar o valor
+      // converter o valor
+      // mostra no campo de dolar
+
+    }
+  }
